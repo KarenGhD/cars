a22b67642e46ae868d05fe7bb5f102d6cb12b9d4
Karen <karen.sn.amb@gmail.com>
2022-08-16 21:04:48 -0300

    voltando para a branch main

--- main.java
+++ main.java
@@ -1,2 +1,16 @@
-public class main {
+
+public class Main {
+    public static void main(String[] arg){
+        Carro c1= new Carro( 4,"9BWHE21JX24860951","2019");
+        c1.getAnoFabricacao();
+        c1.getChassi();
+        c1.getCor();
+        c1.getNumeroPortas();
+
+        c1.setCor("AZUL");
+    }
+
+
+
+
 }
