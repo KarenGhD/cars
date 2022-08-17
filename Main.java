1448687ecf588072bfbb8841d541f70907f123d6
Karen <karen.sn.amb@gmail.com>
2022-08-16 21:23:21 -0300

    adicionadas as saídas

--- Main.java
+++ Main.java
@@ -8,9 +8,10 @@ public class Main {
         c1.getNumeroPortas();
 
         c1.setCor("AZUL");
+        System.out.println(" A quantidade de portas : "+ c1.getNumeroPortas());
+        System.out.println(" O numero do chassi: "+ c1.getChassi());
+        System.out.println(" Ano de fabricação: " +c1.getAnoFabricacao());
+        System.out.println("\u001b[1m \u001b[34m" + " a cor escolhida: " + "\u001b[0m" + c1.getCor());
     }
-
-
-
 
 }
