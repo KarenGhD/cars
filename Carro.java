6df470d82c64b78087402e4f257160c6e02bdd9f
Karen <karen.sn.amb@gmail.com>
2022-08-16 21:01:20 -0300

    adicionando os getters e setters

--- Carro.java
+++ Carro.java
@@ -1,2 +1,32 @@
 public class Carro {
+    private int numeroPortas;
+    private String chassi;
+    private String anoFabricacao;
+    private String cor;
+
+    public Carro( int numeroPortas, String chassi, String anoFabricacao){
+      this.numeroPortas = numeroPortas;
+      this.chassi = chassi;
+      this.anoFabricacao = anoFabricacao;
+     }
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public String getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 }
---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] arg){
+        Carro c1= new Carro( 4,"9BWHE21JX24860951","2019");
+        c1.getAnoFabricacao();
+        c1.getChassi();
+        c1.getCor();
+        c1.getNumeroPortas();
+
+        c1.setCor("AZUL");
+    }
+
+
+
+
+}
